e088427af3c38d65df9b66ab92a8dd46ffc44a94
Aditya Agrawal <aditya.agrawal@sjsu.edu>
2020-03-09 19:36:39 -0700

    message

--- client/src/components/company/JobsCompany.js
+++ client/src/components/company/JobsCompany.js
@@ -3,19 +3,25 @@ import JobPost from './JobPost'
 import JobNavbar from './JobNavbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import JobsView from '../student/Jobs'
-import { Container } from "react-bootstrap";
+import { Container,Row, Col } from "react-bootstrap";
 
 export default class JobsCompany extends Component {
     render() {
         return (
             <Router>
                 <Container>
+                    <Row>
+                    <Col >
                     <JobNavbar className="pad-all"></JobNavbar>
                     <Route exact path="/" component={JobPost} />
+                    </Col>
+                    </Row>
+                    <Row>
                     <Container>
                         <Route exact path="/jobsc" component={JobPost} />
                         <Route exact path="/view" component={JobsView} />
-                    </Container>
+                        </Container>
+                    </Row>
                 </Container>
             </Router>
         )
